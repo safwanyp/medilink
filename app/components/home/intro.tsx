8d98178d7bfde99f2b2dafef033f718c1dfb924d
Safwan Parkar <safwanparkar6@gmail.com>
2023-06-01 16:00:32 +0400

    layout fix

--- app/components/home/intro.tsx
+++ app/components/home/intro.tsx
@@ -8,7 +8,7 @@ export default function Intro() {
             <h3 className="text-4xl font-satoshi-bold-it text-dark-grey">What we do</h3>
             <div className="w-full h-full flex flex-col gap-10">
                 <p className="font-satoshi-med text-dark-grey">
-                Communicating your health-related information to doctors can be overwhelming. “I hope I mentioned everything.”, “Did I tell them about my medications?”, are just a few of the thoughts that go through people’s minds.
+                Communicating your health-related information to doctors can be overwhelming. <span className="font-satoshi-med-it">“I hope I mentioned everything.”</span>, <span className="font-satoshi-med-it">“Did I tell them about my medications?”</span>, are just a few of the thoughts that go through people’s minds.
                 </p>
                 <p className="font-satoshi-med text-dark-grey">
                 With <text><span className="font-satoshi-bold text-dark-grey">Medi</span><span className="font-satoshi-med text-orange-light">Link</span></text>, we strive to eliminate the human error that might creep in while talking about your medical details. <br />But how do we do this?
